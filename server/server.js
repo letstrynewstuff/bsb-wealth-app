1de38e8e87c1bcf1972c0c9c766590beacb29b74
letstrynewstuff <120053023+letstrynewstuff@users.noreply.github.com>
2025-07-13 09:14:17 -0400

    for Vercel frontends

--- server/server.js
+++ server/server.js
@@ -1435,7 +1435,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173",  "https://admin-benningtonstatebkss.vercel.app",
+      "https://benningtonstatebkss.vercel.app"],
     credentials: true,
   },
 });
